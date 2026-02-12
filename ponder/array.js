75851410e7fae34391cca92e471d7dee5b55c79d
willkjj <willkjj@gmail.com>
2026-02-11 21:26:35 -0700

    movies.html

--- ponder/array.js
+++ ponder/array.js
@@ -82,3 +82,25 @@ const shortWords = words.filter(function(word){
     return word.length < 6;
 })
 console.log(shortWords)
+
+const classes = [
+    {name: 'WDD131  ', credits:2},
+    {name: 'CSE170  ', credits:2},
+    {name: 'ITM455  ', credits:4},
+];
+
+let container = document.querySelector('#classes');
+
+classes.forEach(function(item){
+let name = document.createElement('div');
+
+let html = `
+    <span>${item.name}</span>
+    <span>${item.credits}</span>
+
+`
+
+name.innerHTML = html;
+
+container.appendChild(name)
+})
